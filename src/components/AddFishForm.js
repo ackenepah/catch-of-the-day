c09cb6e98b8f47ef5fe868c9f6c7f4841aad4f15
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-11-30 09:26:34 -0800

    Adding Fish via State 1.) Created constructor function that got the initial state of the application. 2.) Added a new method that will search for the state of fishes object. 3.) The addFish takes in a fish as a param and gives it a current date stamp upon clicking the Add Item button. 4.) Then take the input values and add that fish to our fish object and set the state and reset the form fields.

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -3,21 +3,22 @@ import React from 'react';
 class AddFishForm extends React.Component {
   createFish(event) {
     event.preventDefault();
-    console.log('Gonna make some fish!');
 
     const fish = {
       name: this.name.value,
       price: this.price.value,
       status: this.status.value,
       desc: this.desc.value,
-      image: this.image.value,
+      image: this.image.value
     }
-    console.log(fish);
+
+    this.props.addFish(fish);
+    this.fishForm.reset();
   }
 
   render() {
     return(
-      <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+      <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input ref={(input) => this.name = input} type="text" placeholder="Fish Name" />
         <input ref={(input) => this.price = input} type="text" placeholder="Fish Price" />
         <select ref={(input) => this.status = input}>
