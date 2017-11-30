3dc6f7d04400c772c316c5eeded21d280432a2e2
Adrian Kenepah-Martin <ackenepah@gmail.com>
2017-11-30 07:01:51 -0800

    Added in store form and imported our style.css file to our application.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <h1>Store Picker</h1>
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
     )
   }
 }
