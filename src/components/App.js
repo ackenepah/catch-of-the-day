8da6f52871c686047cc4be55c3c6eec34470b5b0
Adrian Kenepah-Martin <ackenepah@gmail.com>
2018-01-03 14:38:22 -0800

    Live State Editing 1.) Created an onChange handler which will trigger once someone starts typing in the input field. 2.) Which then runs our handleChange function and that takes in a copy of the single fish that is being edited and overwrite whatever input field value has changed. 3.) Created an updateFish function which takes the state of handleChange function and passes that data to our updateFish function. 4.) updateFish takes in the key and the updatedFish object and makes a copy of all of the fishes and overwrites the one fish that was updated and then we set our state.

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
 
@@ -60,6 +61,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  updateFish(key, updateFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updateFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -89,8 +96,18 @@ class App extends React.Component {
           </ul>
         </div>
 
-        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params}
+        />
+
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
